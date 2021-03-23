f5af5806c2540e10980857bf59b9a180fd84641d
siyuanpan <pansiyuan.cs@gmail.com>
2021-03-23 10:57:02 +0800

    dv_linear_tetrahedron_dq

--- src/dV_linear_tetrahedron_dq.cpp
+++ src/dV_linear_tetrahedron_dq.cpp
@@ -5,14 +5,34 @@
 #include <quadrature_single_point.h>
 #include <iostream>
 
-void dV_linear_tetrahedron_dq(Eigen::Vector12d &dV, Eigen::Ref<const Eigen::VectorXd> q, 
-                          Eigen::Ref<const Eigen::MatrixXd> V, Eigen::Ref<const Eigen::RowVectorXi> element, double volume,
-                          double C, double D) {
+void dV_linear_tetrahedron_dq(Eigen::Vector12d &dV, Eigen::Ref<const Eigen::VectorXd> q,
+                              Eigen::Ref<const Eigen::MatrixXd> V, Eigen::Ref<const Eigen::RowVectorXi> element, double volume,
+                              double C, double D)
+{
 
-   auto neohookean_linear_tet = [&](Eigen::Vector12d &dV, Eigen::Ref<const Eigen::VectorXd>q, Eigen::Ref<const Eigen::RowVectorXi> element, Eigen::Ref<const Eigen::Vector3d> X) {
-        
+    auto neohookean_linear_tet = [&](Eigen::Vector12d &dV, Eigen::Ref<const Eigen::VectorXd> q, Eigen::Ref<const Eigen::RowVectorXi> element, Eigen::Ref<const Eigen::Vector3d> X) {
+        Eigen::Matrix3d F;
+        Eigen::Matrix34d t;
+        for (int i = 0; i < 4; ++i)
+        {
+            t.col(i) = q.segment<3>(3 * element(i));
+        }
+        Eigen::Matrix43d dphi;
+        dphi_linear_tetrahedron_dX(dphi, V, element, X);
+        F = t * dphi;
+
+        Eigen::Vector9d psi;
+        dpsi_neo_hookean_dF(psi, F, C, D);
+
+        Eigen::Matrix<double, 9, 12> B;
+        for (int i = 0; i < 4; ++i)
+        {
+            B.block(0, 0 + 3 * i, 3, 1) = dphi.row(i).transpose();
+            B.block(3, 1 + 3 * i, 3, 1) = dphi.row(i).transpose();
+            B.block(6, 2 + 3 * i, 3, 1) = dphi.row(i).transpose();
+        }
+        dV = B.transpose() * psi;
     };
 
-    quadrature_single_point(dV, q, element, volume, neohookean_linear_tet);  
-    
+    quadrature_single_point(dV, q, element, volume, neohookean_linear_tet);
 }

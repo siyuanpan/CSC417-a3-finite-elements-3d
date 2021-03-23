39a6f404d989a9edf3b4bec72d103179f09a06ac
siyuanpan <pansiyuan.cs@gmail.com>
2021-03-23 11:16:03 +0800

    mass_matrix_tet

--- src/T_linear_tetrahedron.cpp
+++ src/T_linear_tetrahedron.cpp
@@ -4,30 +4,32 @@
 void T_linear_tetrahedron(double &T, Eigen::Ref<const Eigen::VectorXd> qdot, Eigen::Ref<const Eigen::RowVectorXi> element, double density, double volume)
 {
 
-    double mass = density * volume;
-    double c0 = (1.0 / 10.0) * mass;
-    double c1 = (1.0 / 20.0) * mass;
+    // double mass = density * volume;
+    // double c0 = (1.0 / 10.0) * mass;
+    // double c1 = (1.0 / 20.0) * mass;
 
     Eigen::Matrix1212d M;
-    M.block(0, 0, 3, 3) = c0 * Eigen::Matrix3d::Identity();
-    M.block(0, 3, 3, 3) = c1 * Eigen::Matrix3d::Identity();
-    M.block(0, 6, 3, 3) = c1 * Eigen::Matrix3d::Identity();
-    M.block(0, 9, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(0, 0, 3, 3) = c0 * Eigen::Matrix3d::Identity();
+    // M.block(0, 3, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(0, 6, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(0, 9, 3, 3) = c1 * Eigen::Matrix3d::Identity();
 
-    M.block(3, 0, 3, 3) = c1 * Eigen::Matrix3d::Identity();
-    M.block(3, 3, 3, 3) = c0 * Eigen::Matrix3d::Identity();
-    M.block(3, 6, 3, 3) = c1 * Eigen::Matrix3d::Identity();
-    M.block(3, 9, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(3, 0, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(3, 3, 3, 3) = c0 * Eigen::Matrix3d::Identity();
+    // M.block(3, 6, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(3, 9, 3, 3) = c1 * Eigen::Matrix3d::Identity();
 
-    M.block(6, 0, 3, 3) = c1 * Eigen::Matrix3d::Identity();
-    M.block(6, 3, 3, 3) = c1 * Eigen::Matrix3d::Identity();
-    M.block(6, 6, 3, 3) = c0 * Eigen::Matrix3d::Identity();
-    M.block(6, 9, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(6, 0, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(6, 3, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(6, 6, 3, 3) = c0 * Eigen::Matrix3d::Identity();
+    // M.block(6, 9, 3, 3) = c1 * Eigen::Matrix3d::Identity();
 
-    M.block(9, 0, 3, 3) = c1 * Eigen::Matrix3d::Identity();
-    M.block(9, 3, 3, 3) = c1 * Eigen::Matrix3d::Identity();
-    M.block(9, 6, 3, 3) = c1 * Eigen::Matrix3d::Identity();
-    M.block(9, 9, 3, 3) = c0 * Eigen::Matrix3d::Identity();
+    // M.block(9, 0, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(9, 3, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(9, 6, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    // M.block(9, 9, 3, 3) = c0 * Eigen::Matrix3d::Identity();
+
+    mass_matrix_linear_tetrahedron(M, qdot, element, density, volume);
 
     Eigen::Vector12d v;
     v.segment<3>(0) = qdot.segment<3>(element(0) * 3);
--- src/mass_matrix_linear_tetrahedron.cpp
+++ src/mass_matrix_linear_tetrahedron.cpp
@@ -1,7 +1,29 @@
- 
- #include <mass_matrix_linear_tetrahedron.h>
 
- void mass_matrix_linear_tetrahedron(Eigen::Matrix1212d &M, Eigen::Ref<const Eigen::VectorXd> qdot, Eigen::Ref<const Eigen::RowVectorXi> element, double density, double volume) {
-                
-   
- }
+#include <mass_matrix_linear_tetrahedron.h>
+
+void mass_matrix_linear_tetrahedron(Eigen::Matrix1212d &M, Eigen::Ref<const Eigen::VectorXd> qdot, Eigen::Ref<const Eigen::RowVectorXi> element, double density, double volume)
+{
+    double mass = density * volume;
+    double c0 = (1.0 / 10.0) * mass;
+    double c1 = (1.0 / 20.0) * mass;
+
+    M.block(0, 0, 3, 3) = c0 * Eigen::Matrix3d::Identity();
+    M.block(0, 3, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    M.block(0, 6, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    M.block(0, 9, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+
+    M.block(3, 0, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    M.block(3, 3, 3, 3) = c0 * Eigen::Matrix3d::Identity();
+    M.block(3, 6, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    M.block(3, 9, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+
+    M.block(6, 0, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    M.block(6, 3, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    M.block(6, 6, 3, 3) = c0 * Eigen::Matrix3d::Identity();
+    M.block(6, 9, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+
+    M.block(9, 0, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    M.block(9, 3, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    M.block(9, 6, 3, 3) = c1 * Eigen::Matrix3d::Identity();
+    M.block(9, 9, 3, 3) = c0 * Eigen::Matrix3d::Identity();
+}
